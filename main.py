e4ee480df418be1e531dbcb46a5fc460e4e6c984
CesarArathAP Cesar Arath <1722110585@utectulancingo.edu.mx>
2023-09-29 00:18:25 +0000

    Se logro mostrar los registros csv en formato json

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from fastapi import FastAPI
+import csv
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello Niños"}
 
 @app.get("/v1/contactos")
-async def get_contactos():
-    # TODO read contactos.csv
-    # TODO JSON encode contactos.csv
-    # TODO save in response
-    response = []
+async def contactos():
+    contactos = []
+
+    try:
+        with open("contactos.csv", newline="", encoding="utf-8") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                contactos.append({"nombre": row["nombre"], "email": row["email"]})
+    except FileNotFoundError:
+        return {"error": "El archivo CSV no se encuentra"}
+
+    response = { "contactos": contactos}
     return response
