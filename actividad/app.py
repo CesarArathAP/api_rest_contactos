c92ddc6f527cc122f436869f02b959b4b5ce2440
CesarArathAP Cesar Arath <1722110585@utectulancingo.edu.mx>
2023-10-14 22:44:28 +0000

    Actividad de Evaluación 2.1

--- actividad/app.py
+++ actividad/app.py
@@ -4,10 +4,11 @@ import csv
 from PIL import Image, ImageOps
 from io import BytesIO
 import hashlib
+import qrcode
+import os
 
 app = FastAPI()
 
-# Definir un modelo de datos para el contacto
 class Contacto(BaseModel):
     id_contacto: int
     nombre: str
@@ -16,25 +17,12 @@ class Contacto(BaseModel):
     email: str
     telefono: str
 
-# 1.1 Crear un archivo contactos.csv con 2 registros de prueba
-contactos_data = [
-    {"id_contacto": 1, "nombre": "Juan", "primer_apellido": "Perez", "segundo_apellido": "Lopez", "email": "juan@example.com", "telefono": "123-456-7890"},
-    {"id_contacto": 2, "nombre": "Maria", "primer_apellido": "Gomez", "segundo_apellido": "Rodriguez", "email": "maria@example.com", "telefono": "987-654-3210"}
-]
-
-with open('contactos.csv', mode='w', newline='') as file:
-    fieldnames = ['id_contacto', 'nombre', 'primer_apellido', 'segundo_apellido', 'email', 'telefono']
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerows(contactos_data)
-
-# 1.2 Programar el endpoint GET /contactos
-@app.get('/', response_model=list[Contacto])
+@app.get('/contactos', response_model=list[Contacto])
 def get_contactos():
     try:
         with open('contactos.csv', mode='r') as file:
             reader = csv.DictReader(file)
-            contactos = [row for row in reader]
+            contactos = [Contacto(**row) for row in reader]
 
         return contactos
     except FileNotFoundError:
@@ -42,10 +30,8 @@ def get_contactos():
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-# Directorio para guardar las imágenes
 image_directory = 'static/imagenes/'
 
-# 1.3 Programar el endpoint POST /contactos
 @app.post('/contactos', response_model=Contacto)
 def add_contacto(contacto: Contacto):
     try:
@@ -58,9 +44,7 @@ def add_contacto(contacto: Contacto):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-# 1.4 Programar el endpoint PUT/PATCH /contactos/{id_contacto}
-# @app.put('/contactos/{id_contacto}', response_model=Contacto)
-@app.patch('/contactos/{id_contacto}', response_model=Contacto)
+@app.patch('/contactos/{id}', response_model=Contacto)
 def update_contacto(id_contacto: int, contacto: Contacto):
     try:
         with open('contactos.csv', mode='r') as file:
@@ -92,7 +76,7 @@ def update_contacto(id_contacto: int, contacto: Contacto):
         raise HTTPException(status_code=500, detail=str(e))
 
 # 1.5 Programar el endpoint DELETE /contactos/{id_contacto}
-@app.delete('/contactos/{id_contacto}', response_model=dict)
+@app.delete('/contactos/{id}', response_model=dict)
 def delete_contacto(id_contacto: int):
     try:
         with open('contactos.csv', mode='r') as file:
@@ -135,24 +119,20 @@ def search_contactos(nombre: str = Query(..., title="Nombre a buscar")):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-# 1.7 Programar el endpoint POST /imagenes
 @app.post('/imagenes', response_model=dict)
 def upload_image(image: UploadFile):
     try:
         image_data = Image.open(image.file)
-        # Guardar la imagen en el directorio especificado
         image_data.save(image_directory + image.filename)
         return {"message": "Imagen guardada exitosamente"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-# 1.7.1 Implementar efectos en el endpoint de imágenes
 @app.post('/imagenes/efectos', response_model=dict)
 def apply_image_effects(image: UploadFile, crop: str = Query(None), fliph: bool = Query(False), colorize: bool = Query(False)):
     try:
         image_data = Image.open(image.file)
 
-        # Aplicar efectos según los parámetros
         if crop:
             left, top, right, bottom = map(int, crop.split(','))
             image_data = image_data.crop((left, top, right, bottom))
@@ -163,25 +143,21 @@ def apply_image_effects(image: UploadFile, crop: str = Query(None), fliph: bool 
         if colorize:
             image_data = ImageOps.colorize(image_data, 'red', 'yellow')
 
-        # Guardar la imagen modificada en el directorio especificado
         image_data.save(image_directory + 'modified_' + image.filename)
         return {"message": "Efectos aplicados exitosamente"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+qr_directory = 'static/qrcodes/'
 
-# 1.8 Programar un endpoint adicional, por ejemplo, generador de MD5
-class MD5Input(BaseModel):
-    text: str
-
-class MD5Output(BaseModel):
-    md5_hash: str
-
-@app.post('/md5', response_model=MD5Output)
-def generate_md5(data: MD5Input):
+@app.get('/generar_codigo_qr')
+def generar_codigo_qr(texto: str):
     try:
-        text = data.text
-        md5_hash = hashlib.md5(text.encode()).hexdigest()
-        return {"md5_hash": md5_hash}
+        img = qrcode.make(texto)
+        file_name = f"qr_{texto[0]}.png"
+        file_path = os.path.join(qr_directory, file_name)
+        img.save(file_path)  
+        return {"message": f"Código QR generado exitosamente y guardado en {file_path}"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
